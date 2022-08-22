29f090926a4d29852347c0e1a5ae74ecd6abe2b4
Berkin Aker <berkin.aker@bil.omu.edu.tr>
2022-08-22 10:55:31 +0300

    fixed:product_destroy

--- app/controllers/bid_products_controller.rb
+++ app/controllers/bid_products_controller.rb
@@ -12,7 +12,7 @@ class BidProductsController < ApplicationController
 
   # GET /bid_products/new
   def new
-    @bid_product = BidProduct.new()
+    @bid_product = BidProduct.new(bid_product_form)
     @bid_id = params[:bid_id]
   end
 
@@ -26,7 +26,7 @@ class BidProductsController < ApplicationController
 
     respond_to do |format|
       if @bid_product.save
-        format.html { redirect_to bid_product_url(@bid_product), notice: "Bid product was successfully created." }
+        format.html { redirect_to bid_url(@bid_product.bid_id), notice: "İhaleye ürün başarıyla eklendi." }
         format.json { render :show => "bids/show", status: :created, location: @bid_product }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -67,5 +67,9 @@ class BidProductsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def bid_product_params
       params.require(:bid_product).permit(:bid_id, :product_id, :unit_id, :quantity)
+    end
+
+    def bid_product_form
+      params.permit(:bid_id, :product_id, :unit_id, :quantity)
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  has_one :bid_products
+  has_one :bid_product, :dependent => :destroy
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
